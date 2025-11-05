b715c880ba5186b41baef2acbf2fccd74647e562
Sören Weindel <soeren.weindel@kit.edu>
2025-11-05 17:46:10 +0100

    Fix verification of SPARQL containing literals - Expose to_literal method in package init

--- graph_db_interface/__init__.py
+++ graph_db_interface/__init__.py
@@ -10,6 +10,7 @@ LOGGER.setLevel(logging.WARNING)
 from .sparql_query import SPARQLQuery
 from .graph_db import GraphDB
 from .utils.graph_db_credentials import GraphDBCredentials
+from .utils.utils import to_literal
 from .utils.processing import process_bindings_select
 from .utils.pretty_print import format_result
 
@@ -17,6 +18,7 @@ __all__ = [
     "GraphDB",
     "GraphDBCredentials",
     "SPARQLQuery",
+    "to_literal",
     "process_bindings_select",
     "format_result"
 ]
